1e630abefed8361f91647aa5cd6a133b6588c5e2
Kannan Manickam <kannan@rightscale.com>
2013-03-08 14:07:07 -0800

    refactored some methods and added lots of documentation to the backup method

--- lib/jenkins/backup.rb
+++ lib/jenkins/backup.rb
@@ -37,41 +37,72 @@ module Jenkins
       metadata[:contents] = "jobs"
 
       tmp_dir = Dir.mktmpdir
-      puts "Temp Dir: #{tmp_dir}"
-      # Jobs
+
+      # Collect information about Jobs
       jobs_dir = "#{tmp_dir}/jobs"
       Dir.mkdir(jobs_dir)
       jobs = @client.job.list_all
       metadata[:jobs][:count] = jobs.length
       metadata[:jobs][:names] = jobs
 
+      # Obtain xml configuration of all jobs
       jobs.each do |job|
         puts "Obtaining xml for #{job}"
         xml = @client.job.get_config(job)
         File.open("#{jobs_dir}/#{job}.xml", "w") { |f| f.write(xml) }
       end
 
+      # Collect information Views
       views_dir = "#{tmp_dir}/views"
       Dir.mkdir(views_dir)
       views = @client.view.list
       metadata[:views] = {}
-      metadata[:views][:count] = views.length
+      # Number of views. Don't consider "All". It is the default view.
+      metadata[:views][:count] = views.length - 1
       metadata[:views][:details] = []
+
+      # Obtain specific attributes of each view so it can be created in jenkins
+      # with the same configuration. Posting config.xml to view doesn't seem to
+      # be working now. So let's just create the view by giving all required
+      # attributes.
       views.each do |view|
         next if view == "All"
         puts "Obtaining xml for #{view}"
         xml = @client.view.get_config(view)
         File.open("#{views_dir}/#{view}.xml", "w") { |f| f.write(xml) }
         job_names = @client.view.list_jobs(view)
-        filter_queue = xml.match(/<filterQueue>(.*)<\/filterQueue>/)[1]
-        filter_executors = xml.match(/<filterExecutors>(.*)<\/filterExecutors>/)[1]
-        regex = xml.match(/<includeRegex>(.*)<\/includeRegex>/)[1]
+
+        # Filter Queue attribute
+        filter_queue = xml.match(/<filterQueue>(.*)<\/filterQueue>/)
+        filter_queue = (filter_queue[1] unless filter_queue.nil?)
+
+        # Filter Executors attribute
+        filter_executors = xml.match(/<filterExecutors>(.*)<\/filterExecutors>/)
+        filter_executors = (filter_executors[1] unless filter_executors.nil?)
+
+        # Regex attribute for jobs
+        regex = xml.match(/<includeRegex>(.*)<\/includeRegex>/)
+        regex = (regex[1] unless regex.nil?)
+
+        # Jobs in view that are not matched by regular expression. Any job can
+        # be added to a view, not just the ones that match the regex provided.
+        # So if there are jobs that don't match the regex, those should be
+        # captured separately so they can be added when the backup is restored.
+        # So just list all jobs in a view and skip the ones that match the
+        # regex as they will get added by Jenkins as we mention the regex
+        # attribute while creation.
         unmatched_jobs = []
         job_names.each do |job|
           unless regex && job =~ /#{regex}/
             unmatched_jobs << job
           end
         end
+
+        # Construct the metadata for specific attributes of a view. If a
+        # particular attribute is not found in the view, its value will be nil,
+        # so during restore, if a particular value is nil it should be passed
+        # in to the create call so the default value will be chosen by the
+        # jenkins api client.
         metadata[:views][:details] << {
           :view_name => view,
           :filter_queue => filter_queue,
@@ -81,40 +112,26 @@ module Jenkins
         }
       end
 
-
+      # Write metadata as a YAML file
       File.open("#{tmp_dir}/metadata.yml", "w") { |f| f.write(metadata.to_yaml) }
 
-      Archive.write_open_filename(
+      # Open the archive
+      archive = Archive.write_open_filename(
         "#{name}-#{timestamp.to_i}.tar.gz",
         Archive::COMPRESSION_GZIP,
         Archive::FORMAT_TAR
-      ) do |ar|
-        Dir.glob("#{jobs_dir}/*xml").each do |fn|
-          short_name = fn.split("#{tmp_dir}/").last
-          ar.new_entry do |entry|
-            entry.copy_stat(fn)
-            entry.pathname = short_name
-            ar.write_header(entry)
-            ar.write_data(open(fn) { |f| f.read})
-          end
-        end
-        Dir.glob("#{views_dir}/*xml").each do |fn|
-          short_name = fn.split("#{tmp_dir}/").last
-          ar.new_entry do |entry|
-            entry.copy_stat(fn)
-            entry.pathname = short_name
-            ar.write_header(entry)
-            ar.write_data(open(fn) { |f| f.read})
-          end
-        end
-        metadata_fn = "#{tmp_dir}/metadata.yml"
-        ar.new_entry do |entry|
-          entry.copy_stat(metadata_fn)
-          entry.pathname = "metadata.yml"
-          ar.write_header(entry)
-          ar.write_data(open(metadata_fn) { |f| f.read})
-        end
+      )
+      write_xml_to_archive(archive, jobs_dir, tmp_dir)
+      write_xml_to_archive(archive, views_dir, tmp_dir)
+      metadata_fn = "#{tmp_dir}/metadata.yml"
+      archive.new_entry do |entry|
+        entry.copy_stat(metadata_fn)
+        entry.pathname = "metadata.yml"
+        archive.write_header(entry)
+        archive.write_data(open(metadata_fn) { |f| f.read})
       end
+      # Close the archive
+      archive.close
       FileUtils.rm_rf(tmp_dir)
       puts metadata.inspect
     end
@@ -150,6 +167,18 @@ module Jenkins
     end
 
     private
+
+    def write_xml_to_archive(archive, xml_dir, tmp_dir)
+      Dir.glob("#{xml_dir}/*xml").each do |fn|
+        short_name = fn.split("#{tmp_dir}/").last
+        archive.new_entry do |entry|
+          entry.copy_stat(fn)
+          entry.pathname = short_name
+          archive.write_header(entry)
+          archive.write_data(open(fn) { |f| f.read})
+        end
+      end
+    end
 
     def restore_jobs(jobs_dir, job_metadata)
       job_metadata[:names].each do |job|
